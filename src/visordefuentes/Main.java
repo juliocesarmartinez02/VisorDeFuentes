18b75fa64d4f672863345c68d7a29bdf97f26f19
faryCima <ediraf15_fress@hotmail.com>
2016-06-05 16:16:46 -0500

    julio cesar martinez

---
+++ src/visordefuentes/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package visordefuentes;
+
+import gui.MainForm;
+
+/**
+ *
+ * @author Julio
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainForm Visor=new MainForm();
+        Visor.setVisible(true);
+    }
+    
+}
